57d8b85c787bdb35d942e3d4e2d0337f74850f8e
kev <kevinoduor8877@gmail.com>
2023-09-24 05:06:49 +0300

    fixed buggy line

--- gdb.c
+++ gdb.c
@@ -3,8 +3,9 @@
 int main(int argc, char **argv)
 {
 	int d =2;
-	printf("this program has a bug!\n");
-	scanf("%d", d);
-	printf("You gave me: %d\n");
+	printf("this program has a bug!\n Give me something \n");
+	//scanf should take in the pointer reference to the integer as it expects an integer ref, not an integer value
+	scanf("%d", &d);
+	printf("You gave me: %d \n",d );
 return 0;
 }
